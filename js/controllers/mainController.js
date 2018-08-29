7298e06ce33ef4295fbefdc049651005b11730f8
Alan Ramirez <alanramirez@MacBook-Pro-de-Alan.local>
2018-08-29 17:35:34 -0500

    Generated JSON to process the result of the game

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,10 +1,17 @@
 app.controller('mainController',function($scope){
 
-  $scope.startGame = function(){
-    alert(computerSelection());
-  };
+  $scope.you = 0;
+  $scope.computer = 0;
+
+  $scope.play = function(youSelect){
+    var computerSelect = computerSelection();
+    var result = processGame[youSelect][computerSelect];
+
+    alert("" + youSelect + " -> " + computerSelect + " -> " + result);
+  }
 
   var computerSelection = function(){
+
     var number = Math.floor(Math.random() * 3) + 1;
 
     switch(number) {
@@ -17,7 +24,25 @@ app.controller('mainController',function($scope){
     default:
         return "Scissors";
     }
-    
+
+  };
+
+  var processGame = {
+    "Rock" : {
+      "Rock" : 0,
+      "Paper" : -1,
+      "Scissors" : 1
+    },
+    "Paper" : {
+      "Rock" : 1,
+      "Paper" : 0,
+      "Scissors" : -1
+    },
+    "Scissors" : {
+      "Rock" : -1,
+      "Paper" : 1,
+      "Scissors" : 0
+    }
   };
 
 });
